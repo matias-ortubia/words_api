c0fb16652994268ec720ce36912ecbca279578fe
matiasortubia <matiortubia@gmail.com>
2023-07-06 01:58:09 -0300

    Update settings.py

--- wordle_project/wordle_project/settings.py
+++ wordle_project/wordle_project/settings.py
@@ -11,16 +11,22 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env(DEBUG=(bool, False))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&m2ees^wq13cv72a05x4u^-%3&on_$#qjow_wehh9xn06xo)q5'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,10 +82,14 @@ WSGI_APPLICATION = 'wordle_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': 'words',
+		'USER': env('DB_OWNER'),
+		'PASSWORD': env('DB_PASSWORD'), # La contraseña del usuario postgres
+		'HOST': env('DB_HOST'),
+		'PORT': env('PORT'),
+	}
 }
 
 
